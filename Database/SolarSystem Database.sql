d0f646258593b053be1018531d6d45efa7d16262
SimonOnyszczakHansen <107280264+SimonOnyszczakHansen@users.noreply.github.com>
2023-06-09 12:35:53 +0200

    Add files via upload

---
+++ Database/SolarSystem Database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE SolarSystem
+USE SolarSystem
+
+CREATE TABLE Planets(
+PlanetID int,
+ImagePath VARCHAR(255),
+PlantText text,
+);
+
+CREATE TABLE Galaxies(
+GalaxyID int,
+ImagePath VARCHAR(255),
+GalaxyText text
+);
+
+CREATE TABLE Comets(
+CometID int,
+ImagePath VARCHAR(255),
+CometText text
+);
